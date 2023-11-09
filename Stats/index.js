9fbe6397de34c05dd67ce8ffe369f172802408fe
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-11-09 10:55:01 -0700

    first commit

---
+++ Stats/index.js
@@ -0,0 +1,22 @@
+function getLength(arr) {
+  return arr;
+}
+
+function getSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < getSum.length; i++);
+  {
+    sum + arr;
+  }
+  return arr;
+}
+
+const numbers = [4, 7, 2, 5, 1];
+console.log(`You have given ${getLength(numbers)} numbers.`);
+console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+//console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+//console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+//console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+//console.log(`The range of your numbers is ${getRange(numbers)}.`);
+//console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+//console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
