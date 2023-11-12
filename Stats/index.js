4cad39630e75fe997bea5ee030fb6220692c1d84
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-11-12 15:56:09 -0700

    figured out odds and evens

--- Stats/index.js
+++ Stats/index.js
@@ -1,20 +1,20 @@
-function getLength(arr) {
-  return arr;
+function getLength(numbers) {
+  return numbers;
 }
 
-function getSum(arr) {
+function getSum(numbers) {
   sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
+  for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
   }
 
   return sum;
 }
 
-function getMean(arr) {
+function getMean(numbers) {
   mean = 0;
-  for (let i = 0; i < arr.length; i++) {
-    mean = (arr[i] + sum) / 5;
+  for (let i = 0; i < numbers.length; i++) {
+    mean = (numbers[i] + sum) / 8;
   }
   return mean;
 }
@@ -35,12 +35,36 @@ function getMax(numbers) {
   return max;
 }
 
-const numbers = [4, 7, 1, 5, 3];
+function getRange(numbers) {
+  range = 0;
+  {
+    range = max - min;
+  }
+  return range;
+}
+function getEvens(numbers) {
+  let even = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) even.push(numbers[i]);
+  }
+  return even;
+}
+
+function getOdds(numbers) {
+  let odd = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 != 0) odd.push(numbers[i]);
+  }
+  return odd;
+}
+
+const numbers = [4, 7, 8, 5, 3, 2, 1, 10];
+
 console.log(`You have given ${getLength(numbers)} numbers.`);
 console.log(`The sum of your numbers is ${getSum(numbers)}.`);
 console.log(`The mean of your numbers is ${getMean(numbers)}.`);
 console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
 console.log(`The largest of your numbers is ${getMax(numbers)}.`);
-//console.log(`The range of your numbers is ${getRange(numbers)}.`);
-//console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-//console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+console.log(`The range of your numbers is ${getRange(numbers)}.`);
+console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
